34587feac4b85ff95997562bedff8d5122a51887
User Whitecube7 <whitecube7@www4057.sakura.ne.jp>
2019-02-09 11:32:36 +0900

    file system ui

--- admin/editor.js
+++
@@ -1,5 +0,0 @@
-window.onload = function () {
-  let editFile = "ファイル名<input type='text'><br>編集<textarea></textarea><br><button>登録</button>";
-  let createDirectory = "フォルダ名<input type='text'><button>登録</button> ";
-  document.getElementsByTagName("article")[0].innerHTML = editFile + '<br>' + createDirectory;
-}
--- main/Admin.php
+++ main/Admin.php
@@ -3,13 +3,21 @@
 class Admin {
     function __construct() {
 
-        echo '<script>function visible(i){ document.getElementsByClassName("hidden")[i].style.display="block"; }</script>';
+        echo '<script>function visible(i){';
+        echo ' if (document.getElementsByClassName("button")[i].innerHTML == "閉じる") {';
+        echo 'document.getElementsByClassName("hidden")[i].style.display = "none";';
+        echo 'document.getElementsByClassName("button")[i].innerHTML = "編集";} ';
+        echo 'else {';
+        echo 'document.getElementsByClassName("hidden")[i].style.display="block";';
+        echo 'document.getElementsByClassName("button")[i].innerHTML = "閉じる";';
+        echo '}';
+        echo '}</script>';
         $path = $_SERVER['DOCUMENT_ROOT'];
         $dirs = scandir($path);
         $i = 0;
         foreach ($dirs as $dir) {
             if ($dir == '.' || $dir == '..') { continue; }
-            echo '<div style="width:300px;"><span style="margin-left:100px; " >'.$dir.'</span><button style="float: right;" onclick="visible('.$i.');">編集</button></div><span class="hidden" style="display:none; margin-left:100px;" >';
+            echo '<div style="width:300px;"><span style="margin-left:100px; " >'.$dir.'</span><button class="button" style="float: right;" onclick="visible('.$i.');">編集</button></div><span class="hidden" style="display:none; margin-left:100px;" >';
             if (is_dir($dir)) {
                 $dirs2 = scandir($path.'/'.$dir);
                 foreach ($dirs2 as $dir2) {
@@ -26,9 +34,9 @@ class Admin {
 
         $filename = './memo.php';
  
- //       mkdir('memo');
-        include('./admin/editor.html');
-//        file_put_contents($filename, '<?php echo "ファイル作成しました。"; ');
+//        include('./admin/editor.html');
+//        include('./admin/createDirectory.html');
+          include('./admin/rename.html');       
  
     }
 }
